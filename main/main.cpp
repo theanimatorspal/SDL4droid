3836842d9b4d76b630cf7be4b1ec960d30f01720
Darshan Koirala <theanimatorspal@gmail.com>
2024-01-15 10:18:03 +0545

    SDL Done

--- main/main.cpp
+++ main/main.cpp
@@ -1,40 +1,18 @@
-#include "main.h"
 
-#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "main", __VA_ARGS__))
-#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "main", __VA_ARGS__))
+#include <SDL.h>
 
-extern "C" {
-	/* This trivial function returns the platform ABI for which this dynamic native library is compiled.*/
-	const char * main::getPlatformABI()
-	{
-	#if defined(__arm__)
-	#if defined(__ARM_ARCH_7A__)
-	#if defined(__ARM_NEON__)
-		#define ABI "armeabi-v7a/NEON"
-	#else
-		#define ABI "armeabi-v7a"
-	#endif
-	#else
-		#define ABI "armeabi"
-	#endif
-	#elif defined(__i386__)
-		#define ABI "x86"
-	#else
-		#define ABI "unknown"
-	#endif
-		LOGI("This dynamic shared library is compiled with ABI: %s", ABI);
-		return "This native library is compiled with ABI: %s" ABI ".";
-	}
+int main(int argc, char* argv[])
+{
 
-	void main()
-	{
-	}
+    // returns zero on success else non-zero
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+    }
+    SDL_Window* win = SDL_CreateWindow("GAME",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        1000, 1000, 0);
+    while (1);
 
-	main::main()
-	{
-	}
-
-	main::~main()
-	{
-	}
+    return 0;
 }
